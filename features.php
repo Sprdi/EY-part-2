ee10ba633e4142a8302cba4ee978a86e4eefbe22
IhsanSprdi <ihsansupardi420@gmail.com>
2024-02-05 18:52:25 +0700

    backup

---
+++ features.php
@@ -0,0 +1,271 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Eternity</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Roboto:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: SoftLand
+  * Updated: Sep 18 2023 with Bootstrap v5.3.2
+  * Template URL: https://bootstrapmade.com/softland-bootstrap-app-landing-page-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top d-flex align-items-center">
+    <div class="container d-flex justify-content-between align-items-center">
+
+      <div class="logo">
+        <h1><a href="index.php">Eternity Yours</a></h1>
+        <!-- Uncomment below if you prefer to use an image logo -->
+        <!-- <a href="index.php"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+      </div>
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a class="" href="index.php">Beranda</a></li>
+          <li><a class="active" href="features.php">Fitur</a></li>
+          <li><a href="pricing.php">Harga</a></li>
+          <li><a href="design.php">Design</a></li>
+          <li class="dropdown"><a href="#"><span>Akun Saya</span> <i class="bi bi-chevron-down"></i></a>
+            <ul>
+            <li><a href="wedding-1">Login</a></li>
+            </ul>
+          </li>
+          <li><a href="contact.php">Contact Us</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+    </div>
+  </header><!-- End Header -->
+
+  <main id="main">
+
+    <!-- ======= Features Section ======= -->
+
+    <section class="hero-section inner-page">
+      <div class="wave">
+
+        <svg width="1920px" height="265px" viewBox="0 0 1920 265" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+          <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+            <g id="Apple-TV" transform="translate(0.000000, -402.000000)" fill="#FFFFFF">
+              <path d="M0,439.134243 C175.04074,464.89273 327.944386,477.771974 458.710937,477.771974 C654.860765,477.771974 870.645295,442.632362 1205.9828,410.192501 C1429.54114,388.565926 1667.54687,411.092417 1920,477.771974 L1920,667 L1017.15166,667 L0,667 L0,439.134243 Z" id="Path"></path>
+            </g>
+          </g>
+        </svg>
+
+      </div>
+
+      <div class="container">
+        <div class="row align-items-center">
+          <div class="col-12">
+            <div class="row justify-content-center">
+              <div class="col-md-7 text-center hero-text">
+                <h1 data-aos="fade-up" data-aos-delay="">Fitur Eternity Yours</h1>
+                <p class="mb-5" data-aos="fade-up" data-aos-delay="100">Fitur Fitur Website Undangan Digital Kami</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </section>
+
+    <section class="section pb-0">
+      <div class="container">
+        <div class="row align-items-center">
+          <div class="col-md-4 me-auto">
+            <h2 class="mb-4">Design Keren & Mantap</h2>
+            <p class="mb-4">Fitur "Design Keren" menawarkan kumpulan desain yang menarik dan estetis untuk memberikan tampilan visual yang menawan pada undangan digital Anda. Dengan pilihan desain yang kreatif dan modern, Anda dapat memilih yang paling sesuai dengan tema atau gaya acara Anda.</p>
+          </div>
+          <div class="col-md-6" data-aos="fade-left">
+            <img src="assets/img/design.svg" width="400" height="400" alt="Image" class="img-fluid">
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="section">
+      <div class="container">
+        <div class="row align-items-center">
+          <div class="col-md-4 ms-auto order-2">
+            <h2 class="mb-4">Full Custom Teks</h2>
+            <p class="mb-4">Fitur "Full Custom Teks" memberikan kebebasan penuh dalam menyesuaikan teks undangan sesuai keinginan Anda. Mulai dari memilih jenis huruf hingga warna teks, Anda dapat dengan mudah menyelaraskan pesan Anda dengan gaya dan kepribadian acara Anda.</p>
+          </div>
+          <div class="col-md-6" data-aos="fade-right">
+            <img src="assets/img/custom.svg" width="400" height="400" alt="Image" class="img-fluid">
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="section pb-0">
+      <div class="container">
+        <div class="row align-items-center">
+          <div class="col-md-4 me-auto">
+            <h2 class="mb-4">Navigasi Lokasi</h2>
+            <p class="mb-4">Dengan fitur "Navigasi Lokasi," undangan digital Anda dapat menyertakan peta interaktif yang memungkinkan para tamu dengan mudah menemukan lokasi acara. Ini memberikan kemudahan navigasi dan membantu tamu tiba dengan tepat waktu.</p>
+          </div>
+          <div class="col-md-6" data-aos="fade-left">
+            <img src="assets/img/locate.svg" width="400" height="400" alt="Image" class="img-fluid">
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="section">
+      <div class="container">
+        <div class="row align-items-center">
+          <div class="col-md-4 ms-auto order-2">
+            <h2 class="mb-4">RSVP </h2>
+            <p class="mb-4">Fitur "RSVP" memungkinkan undangan digital untuk menyertakan opsi konfirmasi kehadiran. Para tamu dapat dengan mudah memberikan tanggapan mereka secara online, memudahkan Anda dalam perencanaan jumlah undangan dan persiapan acara.</p>
+          </div>
+          <div class="col-md-6" data-aos="fade-right">
+            <img src="assets/img/wedd2.svg" width="400" height="400" alt="Image" class="img-fluid">
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="section pb-0">
+      <div class="container">
+        <div class="row align-items-center">
+          <div class="col-md-4 me-auto">
+            <h2 class="mb-4">Background Music</h2>
+            <p class="mb-4">Menghadirkan atmosfer yang lebih hidup, fitur "Background Music" memungkinkan Anda menambahkan musik latar belakang yang sesuai dengan tema acara. Ini memberikan nuansa tambahan dan menciptakan pengalaman audiovisual yang lebih kaya.</p>
+          </div>
+          <div class="col-md-6" data-aos="fade-left">
+            <img src="assets/img/music.jpg" width="400" height="400" alt="Image" class="img-fluid">
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="section">
+      <div class="container">
+        <div class="row align-items-center">
+          <div class="col-md-4 ms-auto order-2">
+            <h2 class="mb-4">Galeri Foto </h2>
+            <p class="mb-4">"Galeri Foto" memungkinkan Anda membagikan momen-momen spesial atau foto-foto terkait acara secara langsung melalui undangan digital. Tamu dapat dengan mudah melihat dan berbagi kenangan dari acara tersebut.</p>
+          </div>
+          <div class="col-md-6" data-aos="fade-right">
+            <img src="assets/img/galery.svg" width="400" height="400" alt="Image" class="img-fluid">
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="section pb-0">
+      <div class="container">
+        <div class="row align-items-center">
+          <div class="col-md-4 me-auto">
+            <h2 class="mb-4">Love Stories</h2>
+            <p class="mb-4"> Fitur "Love Stories" memberikan platform untuk berbagi kisah cinta unik dari pasangan yang merayakan acara tersebut. Dengan memasukkan "Love Stories" ke dalam undangan digital, Anda dapat menambahkan sentuhan pribadi dan emosional pada acara tersebut.</p>
+          </div>
+          <div class="col-md-6" data-aos="fade-left">
+            <img src="assets/img/couple.jpg" width="400" height="400" alt="Image" class="img-fluid">
+          </div>
+        </div>
+      </div>
+    </section>
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <footer class="footer" role="contentinfo">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-4 mb-4 mb-md-0">
+          <h3>About Eternity Yours</h3>
+          <p>EternityYours adalah aplikasi pembuatan website undangan online terbaik buatan
+            salah satu anak smk di kota bandung berjurusan pengembangan perangkat lunak dan gim.</p>
+          <p class="social">
+            <a href="https://www.instagram.com/reiikuzoo/"><span class="bi bi-twitter"></span></a>
+            <a href="https://www.instagram.com/reiikuzoo/"><span class="bi bi-facebook"></span></a>
+            <a href="https://www.instagram.com/reiikuzoo/"><span class="bi bi-instagram"></span></a>
+            <a href="https://www.instagram.com/reiikuzoo/"><span class="bi bi-linkedin"></span></a>
+          </p>
+        </div>
+        <div class="col-md-7 ms-auto">
+          <div class="row site-section pt-0">
+            <div class="col-md-4 mb-4 mb-md-0">
+              <h3>Navigasi</h3>
+              <ul class="list-unstyled">
+                <li><a href="#">Harga</a></li>
+                <li><a href="#">Fitur</a></li>
+                <li><a href="#">Design</a></li>
+                <li><a href="#">Contact</a></li>
+              </ul>
+            </div>
+            <div class="col-md-4 mb-4 mb-md-0">
+              <h3>Servis</h3>
+              <ul class="list-unstyled">
+                <li><a href="#">Team</a></li>
+                <li><a href="#">Collaboration</a></li>
+                <li><a href="#">Todos</a></li>
+                <li><a href="#">Events</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="row justify-content-center text-center">
+        <div class="col-md-7">
+          <p class="copyright">&copy; Copyright Eternity Yours. All Rights Reserved</p>
+          <div class="credits">
+            <!--
+            All the links in the footer should remain intact.
+            You can delete the links only if you purchased the pro version.
+            Licensing information: https://bootstrapmade.com/license/
+            Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/buy/?theme=SoftLand
+          -->
+            Designed by <a href="https://www.instagram.com/reiikuzoo/">Muhammad Raihan Faris</a>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </footer>
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,367 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Eternity Yours</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Roboto:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: SoftLand
+  * Updated: Sep 18 2023 with Bootstrap v5.3.2
+  * Template URL: https://bootstrapmade.com/softland-bootstrap-app-landing-page-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top d-flex align-items-center">
+    <div class="container d-flex justify-content-between align-items-center">
+
+      <div class="logo">
+        <h1><a href="index.php">Eternity Yours</a></h1>
+        <!-- Uncomment below if you prefer to use an image logo -->
+        <!-- <a href="index.php"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+      </div>
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a class="active " href="index.php">Beranda</a></li>
+          <li><a href="features.php">Fitur</a></li>
+          <li><a href="pricing.php">Harga</a></li>
+          <li><a href="design.php">Design</a></li>
+          <li class="dropdown"><a href="#"><span>Akun Saya</span> <i class="bi bi-chevron-down"></i></a>
+            <ul>
+              <li><a href="wedding-1">Login</a></li>
+            </ul>
+          </li>
+          <li><a href="contact.php">Contact Us</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+    </div>
+  </header><!-- End Header -->
+
+  <!-- ======= Hero Section ======= -->
+  <section class="hero-section" id="hero">
+
+    <div class="wave">
+
+      <svg width="100%" height="355px" viewBox="0 0 1920 355" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+        <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+          <g id="Apple-TV" transform="translate(0.000000, -402.000000)" fill="#FFFFFF">
+            <path d="M0,439.134243 C175.04074,464.89273 327.944386,477.771974 458.710937,477.771974 C654.860765,477.771974 870.645295,442.632362 1205.9828,410.192501 C1429.54114,388.565926 1667.54687,411.092417 1920,477.771974 L1920,757 L1017.15166,757 L0,757 L0,439.134243 Z" id="Path"></path>
+          </g>
+        </g>
+      </svg>
+
+    </div>
+
+    <div class="container">
+      <div class="row align-items-center">
+        <div class="col-12 hero-text-image">
+          <div class="row">
+            <div class="col-lg-8 text-center text-lg-start">
+              <h1 data-aos="fade-right">Website Undangan</br>Pernikahan Digital</h1>
+              <p class="mb-5" data-aos="fade-right" data-aos-delay="100">EternityYours adalah aplikasi pembuatan website undangan online terbaik buatan
+                salah satu anak smk di kota bandung berjurusan pengembangan perangkat lunak dan gim.</p>
+              <p data-aos="fade-right" data-aos-delay="200" data-aos-offset="-500"><a href="design.php" class="btn btn-outline-white">Buat Sekarang</a></p>
+            </div>
+            <div class="col-lg-4 iphone-wrap">
+              <img src="assets/img/mckp2.png" alt="Image" class="phone-1" data-aos="fade-right">
+              <img src="assets/img/mckp1.png" alt="Image" class="phone-2" data-aos="fade-right" data-aos-delay="200">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </section><!-- End Hero -->
+
+  <main id="main">
+
+    <!-- ======= Home Section ======= -->
+    <section class="section">
+      <div class="container">
+
+        <div class="row justify-content-center text-center mb-5">
+          <div class="col-md-5" data-aos="fade-up">
+            <h2 class="section-heading">Make your invitation more impressive with Eternity Yours</h2>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-md-4" data-aos="fade-up" data-aos-delay="">
+            <div class="feature-1 text-center">
+              <div class="wrap-icon icon-1">
+                <i class="bi bi-people"></i>
+              </div>
+              <h3 class="mb-3">Harga Terjangkau</h3>
+              <p>Harga terbilang cukup murah, sesuai kebutuhan kalian</p>
+            </div>
+          </div>
+          <div class="col-md-4" data-aos="fade-up" data-aos-delay="100">
+            <div class="feature-1 text-center">
+              <div class="wrap-icon icon-1">
+                <i class="bi bi-brightness-high"></i>
+              </div>
+              <h3 class="mb-3">Design Keren</h3>
+              <p>Disertai pilihan design design premium dari anggota kami</p>
+            </div>
+          </div>
+          <div class="col-md-4" data-aos="fade-up" data-aos-delay="200">
+            <div class="feature-1 text-center">
+              <div class="wrap-icon icon-1">
+                <i class="bi bi-bar-chart"></i>
+              </div>
+              <h3 class="mb-3">Proses Cepat</h3>
+              <p>Proses pembuatan sangat cepat, tidak membuang waktu, buat dadakan pun bisa</p>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </section>
+
+    <section class="section">
+
+      <div class="container">
+        <div class="row justify-content-center text-center mb-5" data-aos="fade">
+          <div class="col-md-6 mb-5">
+            <img src="assets/img/wedd1.svg" alt="Image" class="img-fluid">
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-md-4">
+            <div class="step">
+              <span class="number">01</span>
+              <h3>Daftar</h3>
+              <p>Buatlah akun terlebih dulu agar proses pembuatan menjadi mudah.</p>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="step">
+              <span class="number">02</span>
+              <h3>Buat Undangan</h3>
+              <p>Setelah itu pilih design yang di inginkan, lalu buat undangan.</p>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="step">
+              <span class="number">03</span>
+              <h3>Share Undangan</h3>
+              <p>Jika sudah selesai, undangan siap di share kepada keluarga dan teman anda.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </section>
+
+    <section class="section">
+      <div class="container">
+        <div class="row align-items-center">
+          <div class="col-md-4 ms-auto order-2">
+            <h2 class="mb-4">Fitur Eternity Yours</h2>
+            <p class="mb-4">Selamat datang di bagian Fitur kami, di mana kemudahan dan keunikan bertemu untuk menciptakan pengalaman undangan yang tak terlupakan. Kami bangga mempersembahkan sejumlah fitur canggih yang dirancang khusus untuk menyederhanakan dan mempersonalisasi proses pengundangan acara Anda.</p>
+            <p><a href="features.php" class="btn btn-primary">Lihat Sekarang</a></p>
+          </div>
+          <div class="col-md-6" data-aos="fade-right">
+            <img src="assets/img/fiture.svg" alt="Image" class="img-fluid">
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- ======= Testimonials Section ======= -->
+    <section class="section border-top border-bottom">
+      <div class="container">
+        <div class="row justify-content-center text-center mb-5">
+          <div class="col-md-4">
+            <h2 class="section-heading">Reviews From Customers</h2>
+          </div>
+        </div>
+        <div class="row justify-content-center text-center">
+          <div class="col-md-7">
+
+            <div class="testimonials-slider swiper" data-aos="fade-up" data-aos-delay="100">
+              <div class="swiper-wrapper">
+
+                <div class="swiper-slide">
+                  <div class="review text-center">
+                    <p class="stars">
+                      <span class="bi bi-star-fill"></span>
+                      <span class="bi bi-star-fill"></span>
+                      <span class="bi bi-star-fill"></span>
+                      <span class="bi bi-star-fill"></span>
+                      <span class="bi bi-star-fill muted"></span>
+                    </p>
+                    <h3>Best Design!</h3>
+                    <blockquote>
+                      <p>Recommended banget buat kamu yang mau bikin undangan digital,<br/> desainnya menyeseuaikan selera kitaaa.</p>
+                    </blockquote>
+
+                    <p class="review-user">
+                      <img src="assets/img/person_3.jpg" alt="Image" class="img-fluid rounded-circle mb-3">
+                      <span class="d-block">
+                        <span class="text-black">Silwi</span>, &mdash; User
+                      </span>
+                    </p>
+
+                  </div>
+                </div><!-- End testimonial item -->
+
+                <div class="swiper-slide">
+                  <div class="review text-center">
+                    <p class="stars">
+                      <span class="bi bi-star-fill"></span>
+                      <span class="bi bi-star-fill"></span>
+                      <span class="bi bi-star-fill"></span>
+                      <span class="bi bi-star-fill"></span>
+                      <span class="bi bi-star-fill"></span>
+                    </p>
+                    <h3>Like it.</h3>
+                    <blockquote>
+                      <p>Jarang jarang saya kasih bintang lima, tapi untuk kali ini top banget<br/> desain hasilnyaa memuaskan, respon cepattt.</p>
+                    </blockquote>
+
+                    <p class="review-user">
+                      <img src="assets/img/person_1.jpg" alt="Image" class="img-fluid rounded-circle mb-3">
+                      <span class="d-block">
+                        <span class="text-black">Silwo</span>, &mdash; User
+                      </span>
+                    </p>
+
+                  </div>
+                </div><!-- End testimonial item -->
+
+                <div class="swiper-slide">
+                  <div class="review text-center">
+                    <p class="stars">
+                      <span class="bi bi-star-fill"></span>
+                      <span class="bi bi-star-fill"></span>
+                      <span class="bi bi-star-fill"></span>
+                      <span class="bi bi-star-fill"></span>
+                      <span class="bi bi-star-fill muted"></span>
+                    </p>
+                    <h3>Nice Design</h3>
+                    <blockquote>
+                      <p>Recommended!! hasil desain dan undangan sangat bagus<br/> sesuai dengan yang kita inginkan, seller nya baikk ramahh,<br/> pengerjaannya pun cepatt.</p>
+                    </blockquote>
+
+                    <p class="review-user">
+                      <img src="assets/img/person_1.jpg" alt="Image" class="img-fluid rounded-circle mb-3">
+                      <span class="d-block">
+                        <span class="text-black">Salwa</span>, &mdash; User
+                      </span>
+                    </p>
+
+                  </div>
+                </div><!-- End testimonial item -->
+
+              </div>
+              <div class="swiper-pagination"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section><!-- End Testimonials Section -->
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <footer class="footer" role="contentinfo">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-4 mb-4 mb-md-0">
+          <h3>About Eternity Yours</h3>
+          <p>EternityYours adalah aplikasi pembuatan website undangan online terbaik buatan
+            salah satu anak smk di kota bandung berjurusan pengembangan perangkat lunak dan gim.</p>
+          <p class="social">
+            <a href="https://www.instagram.com/reiikuzoo/"><span class="bi bi-twitter"></span></a>
+            <a href="https://www.instagram.com/reiikuzoo/"><span class="bi bi-facebook"></span></a>
+            <a href="https://www.instagram.com/reiikuzoo/"><span class="bi bi-instagram"></span></a>
+            <a href="https://www.instagram.com/reiikuzoo/"><span class="bi bi-linkedin"></span></a>
+          </p>
+        </div>
+        <div class="col-md-7 ms-auto">
+          <div class="row site-section pt-0">
+            <div class="col-md-4 mb-4 mb-md-0">
+              <h3>Navigasi</h3>
+              <ul class="list-unstyled">
+                <li><a href="#">Harga</a></li>
+                <li><a href="#">Fitur</a></li>
+                <li><a href="#">Design</a></li>
+                <li><a href="#">Contact</a></li>
+              </ul>
+            </div>
+            <div class="col-md-4 mb-4 mb-md-0">
+              <h3>Servis</h3>
+              <ul class="list-unstyled">
+                <li><a href="#">Team</a></li>
+                <li><a href="#">Collaboration</a></li>
+                <li><a href="#">Todos</a></li>
+                <li><a href="#">Events</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="row justify-content-center text-center">
+        <div class="col-md-7">
+          <p class="copyright">&copy; Copyright Eternity Yours. All Rights Reserved</p>
+          <div class="credits">
+            <!--
+            All the links in the footer should remain intact.
+            You can delete the links only if you purchased the pro version.
+            Licensing information: https://bootstrapmade.com/license/
+            Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/buy/?theme=SoftLand
+          -->
+            Designed by <a href="https://www.instagram.com/reiikuzoo/">Muhammad Raihan Faris</a>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </footer>
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
